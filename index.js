c536dcdcc9194c50d7ee4f1ff0ade657ee1a359f
Amit <amit@cocycles.com>
2018-02-07 18:06:28 +0200

    typo fix

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const path = require('path');
 const compiler = require('vue-template-compiler');
 const cabinet = require('filing-cabinet');
 
-module.exports = function(partial, filename, directory, config, webpackConfig, configPath, ast, type) {
+module.exports = function(partial, filename, directory, config, webpackConfig, configPath, ast, isScript) {
   const fileContent = fs.readFileSync(filename);
   const { script, styles } = compiler.parseComponent(fileContent.toString(), { pad: 'line' });
-  if (type === 'script') {
-    const scriptExt = script.lang || 'js';
+  if (isScript) {
+    scriptExt = script.lang || '.js';
     const scriptResult = cabinet({
       partial: partial,
-      filename: `${path.basename(filename)}.${scriptExt}`,
+      filename: path.basename(filename) + scriptExt,
       directory: directory,
       content: script.content
     });
     return scriptResult;
   }
   const stylesResult = styles.map(style => {
-      const styleExt = (style.lang === 'css' || !style.lang) ? 'scss' : style.lang;
-  return cabinet({
-    partial: partial,
-    filename: `${path.basename(filename)}.${styleExt}`,
-    directory: directory,
-    content: style.content
-  })
-});
+    const styleExt = (style.lang === 'css' || !style.lang) ? '.scss' : style.lang;
+    return cabinet({
+      partial: partial,
+      filename: path.basename(filename) + styleExt,
+      directory: directory,
+      content: style.content
+    })
+  });
   return stylesResult[0];
 };
