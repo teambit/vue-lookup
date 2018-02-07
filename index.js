a1719c898a4e273e8d4fb0e663e39a4051d3308b
Amit <amit@cocycles.com>
2018-02-07 23:09:46 +0200

    fix dir path

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const compiler = require('vue-template-compiler');
-const cabinet = require('filing-cabinet');
 
 module.exports = function(partial, filename, directory, config, webpackConfig, configPath, ast, isScript) {
+  const cabinet = require('filing-cabinet');
+
   const fileContent = fs.readFileSync(filename);
   const { script, styles } = compiler.parseComponent(fileContent.toString(), { pad: 'line' });
   if (isScript) {
     const scriptExt = script.lang || '.js';
     return cabinet({
       partial: partial,
-      filename: path.basename(filename) + scriptExt,
-      directory: directory,
+      filename: `${path.basename(filename)}.${scriptExt}`,
+      directory: path.dirname(filename),
       content: script.content
     });
   }
   const stylesResult = styles.map(style => {
-    const styleExt = (style.lang === 'css' || !style.lang) ? '.scss' : style.lang;
+    const styleExt = (style.lang === 'css' || !style.lang) ? 'scss' : style.lang;
     return cabinet({
       partial: partial,
-      filename: path.basename(filename) + styleExt,
-      directory: directory,
+      filename: `${path.basename(filename)}.${styleExt}`,
+      directory: path.dirname(filename),
       content: style.content
     })
   });
