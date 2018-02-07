fe9874e17a711ba9241fced253887562ba6d0cac
Amit <amit@cocycles.com>
2018-02-07 16:03:33 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const compiler = require('vue-template-compiler');
+const cabinet = require('filing-cabinet');
+
+module.exports = function(partial, filename, directory, config, webpackConfig, configPath, ast, type) {
+  const fileContent = fs.readFileSync(filename);
+  const { script, styles } = compiler.parseComponent(fileContent.toString(), { pad: 'line' });
+  if (type === 'script') {
+    scriptExt = script.lang || '.js';
+    const scriptResult = cabinet({
+      partial: partial,
+      filename: path.basename(filename) + scriptExt,
+      directory: directory,
+      content: script.content
+    });
+    return scriptResult;
+  }
+  const stylesResult = styles.map(style => {
+      const styleExt = (style.lang === 'css' || !style.lang) ? '.scss' : style.lang;
+  return cabinet({
+    partial: partial,
+    filename: path.basename(filename) + styleExt,
+    directory: directory,
+    content: style.content
+  })
+});
+  return stylesResult[0];
+};
